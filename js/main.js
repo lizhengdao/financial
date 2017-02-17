e207e90c7ace21483a5ec11aae09a697163379fe
nongweiyi <nongweiyilady@163.com>
2017-02-17 16:43:57 +0800

    将轮播插件unslider换成owl

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 /**
  * Created by weiyi on 2017/1/19.
  */
-//$(function() {
-//    $('.banner').unslider({
-//        autoplay:true,
-//        speed: 500,               //  The speed to animate each slide (in milliseconds)
-//        delay: 2000,
-//        complete: function() {},  //  A function that gets called after every slide animation
-//        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-//        dots: true,               //  Display dot navigation
-//        fluid: true              //  Support responsive design. May break non-responsive designs
-//    });
-//});
 
-$(function() {
-    $('.banner').unslider({
-        autoplay:true,
-        speed: 500,
-        delay: 3000,
-        dots: true,
-        fluid: true
+//$(function () {
+//    $('.banner').unslider({
+//        autoplay: true,
+//        speed: 500,
+//        delay: 3000,
+//        dots: true,
+//        fluid: true
+//    });
+//
+//
+//
+//});
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
     });
 });
